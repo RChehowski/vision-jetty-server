8dc9d6d8ed9f8a5f6fdc86f945ca06e7559612c1
Никита Валявко <septor@tut.by>
2016-07-03 13:40:00 +0300

    Обновил настройки сборщика

--- src/main/java/com/vizor/visionserver/Main.java
+++ src/main/java/com/vizor/visionserver/Main.java
@@ -4,23 +4,7 @@ package com.vizor.visionserver;
  * Created by netherwire on 5/23/16.
  */
 
-import javax.servlet.AsyncContext;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.ServletException;
-import java.io.File;
-import java.io.IOException;
-import java.io.RandomAccessFile;
-import java.nio.ByteBuffer;
-import java.nio.channels.FileChannel;
-import java.nio.file.StandardOpenOption;
-import org.eclipse.jetty.util.Callback;
-
-import org.eclipse.jetty.http.MimeTypes;
-import org.eclipse.jetty.server.HttpOutput;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.Request;
-import org.eclipse.jetty.server.handler.AbstractHandler;
 
 public class Main
 {
